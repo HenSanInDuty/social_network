f8da66298e345451093235da59c344ccb35f17a0
HenSanInDuty <heninduty@gmail.com>
2022-10-25 13:17:01 +0700

    Init Post

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views import generic
 
 from posts.models import Post
+from posts.forms import CreatePostForm
 # Create your views here.
-class CreatePost(generic.CreateView):
-    model = Post
-    fields = ['content']
+class CreatePost(generic.ListView):
     template_name = 'createPost.html'
+    paginate_by = 10
     
+    def get_queryset(self) :
+        return ['1','2','3']
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CreatePostForm
+        context['non'] = "non"
+        return context
