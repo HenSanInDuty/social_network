c19fe82cd111cb92dc7d95e641b30a517a8a3939
HenSanInDuty <heninduty@gmail.com>
2022-10-31 13:39:23 +0700

    Fix chrome environment

--- static/commonJS.js
+++ static/commonJS.js
@@ -1,12 +1,11 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
 })
 
 //Fix link can't link
 var dropDownLinkList = document.querySelectorAll('a.dropdown-item')
-dropDownLinkList.forEach((link)=>{
-  link.addEventListener('click',()=>{
-    window.location.replace(link.href)
-  })
+dropDownLinkList.forEach((link) => {
+    link.addEventListener('click', () => {
+        window.location.replace(link.href)
+    })
 })
